c122ccb4db686a6115f7796cb9e5b2ec881bd722
Jithin K Jose <jithin.jude68@gmail.com>
2018-04-06 17:56:23 +0530

    Initial commit

---
+++ app/src/main/java/com/inc/mountzoft/giscommunity/Login.java
@@ -0,0 +1,20 @@
+package com.inc.mountzoft.giscommunity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signInBtnFunction(View view){
+        Intent intent = new Intent(this,NavigationDrawer.class);
+        startActivity(intent);
+    }
+}
